78dabad4dadb37eef7085585de43e8cb419211bc
Eleazar Federio <eleazarfederio@rocketmail.com>
2021-05-08 00:07:17 +0800

    get chat from firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,6 +12,16 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('It Works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance.collection('chats/yGcYYHnVCHohMeGyEhcc/messages')
+          .snapshots()
+          .listen((data) {
+            print(data.docs[0]['text']);
+          });
+        },
       ),
     );
   }
