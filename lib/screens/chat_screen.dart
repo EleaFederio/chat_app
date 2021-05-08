158e90419ac8df5a5cc11a6356c6b8332b2b6d79
Eleazar Federio <eleazarfederio@rocketmail.com>
2021-05-08 23:28:39 +0800

    show firebase data in the list

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -6,21 +6,30 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('It Works'),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('chats/yGcYYHnVCHohMeGyEhcc/messages')
+            .snapshots(),
+        builder: (context, streamSnapshot) {
+          // show loading sign while waiting for firebase data
+          if(streamSnapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.docs;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          FirebaseFirestore.instance.collection('chats/yGcYYHnVCHohMeGyEhcc/messages')
-          .snapshots()
-          .listen((data) {
-            print(data.docs[0]['text']);
-          });
+
         },
       ),
     );
